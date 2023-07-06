84b0e1bd992c7b38fad20af5e2e7d04f54ef9589
Lerian Febriana <kanglerian@MJLQ2s-MBP.lan>
2023-07-06 09:48:25 +0700

    app region

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const regionRouter = require('./routes/region');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/region', regionRouter);
+
+module.exports = app;
---
+++ routes/region.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+const apiAdapter = require('./apiAdapter');
+
+const { URL_SERVICE_WILAYAH } = process.env;
+const api = apiAdapter(URL_SERVICE_WILAYAH);
+
+router.get('/provinces', async(req, res) => {
+  try {
+      const provinces = await api.get('/provinces');
+      return res.json(provinces.data);
+  } catch (error) {
+      if(error.code === 'ECONNREFUSED'){
+          return res.status(500).json({ status: 'error', message: 'service unavailable' });
+      }
+      const { status, data } = error.response;
+      return res.status(status).json(data);
+  }
+});
+
+router.get('/regencies/:id', async(req, res) => {
+  try {
+      const regencies = await api.get(`/regencies/${req.params.id}`);
+      return res.json(regencies.data);
+  } catch (error) {
+      if(error.code === 'ECONNREFUSED'){
+          return res.status(500).json({ status: 'error', message: 'service unavailable' });
+      }
+      const { status, data } = error.response;
+      return res.status(status).json(data);
+  }
+});
+
+router.get('/districts/:id', async(req, res) => {
+  try {
+      const districts = await api.get(`/districts/${req.params.id}`);
+      return res.json(districts.data);
+  } catch (error) {
+      if(error.code === 'ECONNREFUSED'){
+          return res.status(500).json({ status: 'error', message: 'service unavailable' });
+      }
+      const { status, data } = error.response;
+      return res.status(status).json(data);
+  }
+});
+
+router.get('/villages/:id', async(req, res) => {
+  try {
+      const villages = await api.get(`/villages/${req.params.id}`);
+      return res.json(villages.data);
+  } catch (error) {
+      if(error.code === 'ECONNREFUSED'){
+          return res.status(500).json({ status: 'error', message: 'service unavailable' });
+      }
+      const { status, data } = error.response;
+      return res.status(status).json(data);
+  }
+});
+
+module.exports = router;
