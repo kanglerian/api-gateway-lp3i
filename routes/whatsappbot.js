50fe3f5f77f63051108fdd9ca68103ad588b916d
Lerian Febriana <kanglerian@MJLQ2s-MacBook-Pro.local>
2023-07-10 13:50:50 +0700

    report

--- routes/whatsappbot.js
+++ routes/whatsappbot.js
@@ -4,30 +4,43 @@ const apiAdapter = require('./apiAdapter');
 
 const api = apiAdapter('http://103.163.110.11:4002/');
 
-router.get('/', async(req, res) => {
-  try {
-      const whatsapp = await api.get('/');
-      return res.send(whatsapp.data);
-  } catch (error) {
-      if(error.code === 'ECONNREFUSED'){
-          return res.status(500).json({ status: 'error', message: 'service unavailable' });
-      }
-      const { status, data } = error.response;
-      return res.status(status).json(data);
-  }
-});
-
-router.post('/send', async(req, res) => {
+router.get('/', async (req, res) => {
     try {
-        const whatsapp = await api.post('/send', req.body);
-        return res.json(whatsapp.data);
+        const whatsapp = await api.get('/');
+        return res.send(whatsapp.data);
     } catch (error) {
-        if(error.code === 'ECONNREFUSED'){
+        if (error.code === 'ECONNREFUSED') {
             return res.status(500).json({ status: 'error', message: 'service unavailable' });
         }
         const { status, data } = error.response;
         return res.status(status).json(data);
     }
-  });
+});
+
+router.post('/send', async (req, res) => {
+    try {
+        const whatsapp = await api.post('/send', req.body);
+        return res.json(whatsapp.data);
+    } catch (error) {
+        if (error.code === 'ECONNREFUSED') {
+            return res.status(500).json({ status: 'error', message: 'service unavailable' });
+        }
+        const { status, data } = error.response;
+        return res.status(status).json(data);
+    }
+});
+
+router.post('/report', async (req, res) => {
+    try {
+        const whatsapp = await api.post('/report', req.body);
+        return res.json(whatsapp.data);
+    } catch (error) {
+        if (error.code === 'ECONNREFUSED') {
+            return res.status(500).json({ status: 'error', message: 'service unavailable' });
+        }
+        const { status, data } = error.response;
+        return res.status(status).json(data);
+    }
+});
 
 module.exports = router;
