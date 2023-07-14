e9dcbf9a57971e84a9506486e6e2363b01e8a0a7
Lerian Febriana <kanglerian@MJLQ2s-MBP.lan>
2023-07-14 13:51:34 +0700

    madinah

--- routes/pmbonline.js
+++ routes/pmbonline.js
@@ -19,8 +19,7 @@ router.get('/', async (req, res) => {
 
 router.get('/download/:identity/:filename', async (req, res) => {
     try {
-        const pmb = await api.get(`/download/${req.params.identity}/${req.params.filename}`);
-        return res.send(pmb.data);
+        return await api.get(`/download/${req.params.identity}/${req.params.filename}`);
     } catch (error) {
         if (error.code === 'ECONNREFUSED') {
             return res.status(500).json({ status: 'error', message: 'service unavailable' });
