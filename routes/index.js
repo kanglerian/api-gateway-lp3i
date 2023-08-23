c2624220fb8e35c72a62c84eece3b527c877cded
kanglerian <kanglerian@gmail.com>
2023-08-23 13:22:33 +0700

    tambah Fix

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.send('API Gateway LP3I 🇮🇩');
+    res.send('API Gateway LP3I FIX 🇮🇩');
 });
 
 module.exports = router;
