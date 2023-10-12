bfc3041123c2a3a24531ee10baeb87d03c744e9c
kanglerian <kanglerian@gmail.com>
2023-10-12 16:15:29 +0700

    delete

--- routes/pmbonline.js
+++ routes/pmbonline.js
@@ -47,7 +47,7 @@ router.post('/pmbupload', async (req, res) => {
     }
 });
 
-router.post('/removefile', async (req, res) => {
+router.delete('/removefile', async (req, res) => {
     try {
         console.log(req.body);
         const pmb = await api.post('/remove', req.body);
