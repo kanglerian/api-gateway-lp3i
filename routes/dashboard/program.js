2eb8550a1e8b1bfc50a061f6ac375dfde2020473
Lerian Febriana, A.Md.Kom <kanglerian@gmail.com>
2024-04-01 15:33:29 +0700

    sip

---
+++ routes/dashboard/program.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const apiAdapter = require('../apiAdapter');
+
+const api = apiAdapter('http://103.163.111.39:2001/program');
+
+router.get('/', async (req, res) => {
+    try {
+        const responses = await api.get('/');
+        return res.send(responses.data);
+    } catch (error) {
+        if (error.code === 'ECONNREFUSED') {
+            return res.status(500).json({ status: 'error', message: 'service unavailable' });
+        }
+        return res.status(500).json({ error: "Terjadi kesalahan pada server." });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const responses = await api.post('/', req.body);
+        return res.json(responses.data);
+    } catch (error) {
+        if (error.code === 'ECONNREFUSED') {
+            return res.status(500).json({ status: 'error', message: 'service unavailable' });
+        }
+        return res.status(500).json({ error: "Terjadi kesalahan pada server." });
+    }
+}); 
+
+
+module.exports = router;
