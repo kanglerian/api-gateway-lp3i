22d3d770b303be89132f566d5c68314bca9617ca
Lerian Febriana, A.Md.Kom <kanglerian@gmail.com>
2024-04-02 09:18:27 +0700

    jadi

--- routes/dashboard/program.js
+++ routes/dashboard/program.js
@@ -7,7 +7,7 @@ const api = apiAdapter('http://103.163.111.39:2001/program');
 router.get('/', async (req, res) => {
     try {
         const responses = await api.get('/');
-        return res.json(responses);
+        return res.json(responses.data);
     } catch (error) {
         if (error.code === 'ECONNREFUSED') {
             return res.status(500).json({ status: 'error', message: 'service unavailable' });
