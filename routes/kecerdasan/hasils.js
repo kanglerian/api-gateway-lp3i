cff91005ced4aeee8d84236f57f4de4f0f2cee51
Lerian Febriana, A.Md.Kom <kanglerian@gmail.com>
2024-05-04 14:10:47 +0700

    download

--- routes/kecerdasan/hasils.js
+++ routes/kecerdasan/hasils.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const ExcelJS = require('exceljs');
 const router = express.Router();
 const apiAdapter = require('../apiAdapter');
 
@@ -6,8 +7,30 @@ const api = apiAdapter('http://103.163.111.39:8001/hasils');
 
 router.get('/', async (req, res) => {
     try {
-        await api.get('/');
-        return res.send('Terunduh!');
+        const response = await api.get('/');
+        const results = response.data;
+
+        const workbook = new ExcelJS.Workbook();
+        const sheet = workbook.addWorksheet('Hasil Kecerdasan');
+
+        sheet.addRow(['No.','Sekolah', 'Kelas', 'Nama Lengkap', 'No. Telpon', 'Kecerdasan']);
+
+        results.forEach((result, index) => {
+            sheet.addRow([
+                index + 1,
+                `${result.school}`,
+                `${result.classes}`,
+                `${result.name_user}`,
+                `${result.phone}`,
+                result.jenis_kecerdasan
+            ]);
+        });
+
+        res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        res.setHeader('Content-Disposition', 'attachment; filename="example.xlsx"');
+
+        const buffer = await workbook.xlsx.writeBuffer();
+        res.send(buffer);
     } catch (error) {
         if (error.code === 'ECONNREFUSED') {
             return res.status(500).json({ status: 'error', message: 'service unavailable' });
