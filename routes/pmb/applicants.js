60fdc75212c3b302e7f0a9a69b0d7ab9518dcce3
Lerian Febriana <kanglerian@gmail.com>
2024-09-30 10:15:52 +0700

    applicant

--- routes/pmb/applicants.js
+++ routes/pmb/applicants.js
@@ -25,6 +25,26 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:identity', async (req, res) => {
+  try {
+    const response = await api.get(`/${req.params.identity}`,{
+      headers: {
+        'lp3i-api-key': req.headers['lp3i-api-key'],
+      }
+    });
+    return res.status(200).json(response.data);
+  } catch (error) {
+    if (error.code === 'ECONNREFUSED') {
+      return res.status(500).json({ status: 'error', message: 'service unavailable' });
+    } else if (error.code === 'ERR_HTTP_INVALID_HEADER_VALUE') {
+      return res.status(401).json({ status: 'error', message: 'Headers are missing!' });
+    } else {
+      const response = error.response;
+      return res.status(response.status).json(response.data);
+    }
+  }
+});
+
 /* Use for PPO */
 router.patch('/update/v1/:identity', async (req, res) => {
   try {
